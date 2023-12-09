2b1e3c9be48362a7446686df32b41d49326823bf
clankweedmuff1n <joukeecz@gmail.com>
2023-12-10 01:30:58 +0300

    feat: initial commit

---
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -0,0 +1,54 @@
+import {ConstructorElement, CurrencyIcon, DragIcon, Button} from '@ya.praktikum/react-developer-burger-ui-components';
+import React from 'react';
+import stylesConstructor from './BurgerConstructor.module.css';
+import {data} from "../../utils/data";
+
+function BurgerConstructor() {
+    return (
+        <section className={`${stylesConstructor.constructor} ml-10 mt-20`}>
+            <div className={stylesConstructor.constructor__container}>
+                <ConstructorElement
+                    text='Краторная булка N-200i (верх)'
+                    type='top'
+                    price={200}
+                    thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                    isLocked={true}
+
+                />
+
+                <ul className={`${stylesConstructor.constructor__list} custom-scroll`}>
+                    {data.map((item) => {
+                            return (
+                                <li className={`${stylesConstructor.constructor__item} mt-4 pr-5`}>
+                                    <DragIcon/>
+                                    <ConstructorElement
+                                        text={item.name}
+                                        price={item.price}
+                                        thumbnail={item.image}
+                                    />
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
+
+                <ConstructorElement
+                    text='Краторная булка N-200i (низ)'
+                    thumbnail={'https://code.s3.yandex.net/react/code/bun-02-mobile.png'}
+                    type='bottom'
+                    isLocked={true}
+                    price={200}
+                />
+            </div>
+            <div className={`${stylesConstructor.constructor__total} mr-8 mt-10`}>
+                <div className={`${stylesConstructor.constructor__price} mr-10`}>
+                    <p className='text text_type_digits-medium mr2'>10000</p>
+                    <CurrencyIcon/>
+                </div>
+                <Button type='primary' size='large'>Оформить заказ</Button>
+            </div>
+        </section>
+    )
+}
+
+export default BurgerConstructor;
---
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -0,0 +1,37 @@
+import burgerIngredientsStyles from './BurgerIngredients.module.css';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import React from "react";
+import PropTypes from "prop-types";
+import IngredientsList from "./IngredientsList/IngredientsList";
+import categories from "../../utils/categories";
+
+function BurgerIngredients() {
+    const [current, setCurrent] = React.useState('first');
+    return (
+        <section className={burgerIngredientsStyles.ingredients__section}>
+            <h1 className='text text_type_main-large mb-5'>Соберите бургер</h1>
+            <div className={burgerIngredientsStyles.ingredients__menu}>
+                <Tab value='first' active={current === 'first'} onClick={setCurrent}>
+                    Булки
+                </Tab>
+                <Tab value='second' active={current === 'second'} onClick={setCurrent}>
+                    Соусы
+                </Tab>
+                <Tab value='third' active={current === 'third'} onClick={setCurrent}>
+                    Начинки
+                </Tab>
+            </div>
+            <ul className={`${burgerIngredientsStyles.ingredients__list} custom-scroll`}>
+                {categories.map(category => (
+                    <IngredientsList category={category}/>
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+BurgerIngredients.propTypes = {
+    category: PropTypes.objectOf(PropTypes.arrayOf(categories.isRequired))
+}
+
+export default BurgerIngredients;
